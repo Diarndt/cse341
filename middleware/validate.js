b818cfb6eabf8686eb7e9c965adb1207c372d9ab
Diarndt <diarndt36d@gmail.com>
2023-05-27 21:01:43 -0700

    Week 6 commit

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,16 +1,16 @@
 const validator = require('../helpers/validate');
 
-const saveVeggie = (req, res, next) => {
+const saveVeggie = async (req, res, next) => {
     const validationRule = {
-    plantName: 'required|string', 
-    scientificName: 'string', 
-    countryOfOrigin: 'string', 
-    bestEnvironment: 'string',
-    bestTimeToPlant: 'string', 
-    whenToHarvest: 'string',
-    commonRecipes: 'string'
+    'plantName': 'required|string', 
+    'scientificName': 'string', 
+    'countryOfOrigin': 'string', 
+    'bestEnvironment': 'string',
+    'bestTimeToPlant': 'string', 
+    'whenToHarvest': 'string',
+    'commonRecipes': 'string'
     };
-    validator(req.body, validationRule, {}, (err, status) => {
+    await validator(req.body, validationRule, {}, (err, status) => {
         if(!status) {
             res.status(412).send({
                 success: false,
