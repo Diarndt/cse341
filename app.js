cf316246697ec4638fb1bb732b1974b094c7a5b8
Diarndt <diarndt36d@gmail.com>
2023-06-10 23:54:57 -0700

    Week 8 commit12

--- app.js
+++ app.js
@@ -14,24 +14,6 @@ const port = process.env.PORT;
 
 const app = express();
 
-// const config = {
-//     authRequired: false,
-//     auth0Logout: true,
-//     secret: process.env.SECRET,
-//     baseURL: process.env.BASE_URL,
-//     clientID: process.env.CLIENT_ID,
-//     issuerBaseURL: process.env.ISSUER
-//   };
-  
-//   // auth router attaches /login, /logout, and /callback routes to the baseURL
-//   app.use(auth(config));
-  
-//   // req.isAuthenticated is provided from the auth router
-//   app.get('/', (req, res) => {
-//     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-//   });
-
-
 app
     .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
     .use(bodyParser.json())
